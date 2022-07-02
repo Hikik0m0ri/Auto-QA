be7001a74008274d91610b6f6b5538129832e5c7
Hikik0m0ri <artem.sarkisian517@gmail.com>
2022-07-02 22:17:11 +0300

    AppleInsiderTest

---
+++ src/main/java/AppleInsider/MainPage.java
@@ -0,0 +1,26 @@
+package AppleInsider;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class MainPage {
+    private final SelenideElement textBoxInput = $x("//input[@type='text']");
+
+    public MainPage(String url) {
+        Selenide.open(url);
+    }
+
+    /**
+     * Выполняется поиск по стайту с нажатием кнопки Enter
+     *
+     * @param searchString
+     */
+    public SearchPage search(String searchString) {
+        textBoxInput.setValue(searchString);
+        textBoxInput.sendKeys(Keys.ENTER);
+        return new SearchPage();
+    }
+}
---
+++ src/main/java/AppleInsider/SearchPage.java
@@ -0,0 +1,18 @@
+package AppleInsider;
+
+import com.codeborne.selenide.ElementsCollection;
+
+import static com.codeborne.selenide.Selenide.$$x;
+
+public class SearchPage {
+    private final ElementsCollection articleTitles = $$x("//h2//a");
+
+    /**
+     * Возвращает href из 1 элемента поиска
+     *
+     * @return
+     */
+    public String getHrefFromFirstArticle() {
+        return articleTitles.first().getAttribute("href");
+    }
+}
